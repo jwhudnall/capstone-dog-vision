c79d52618f04bd43adbf50a113ce00b31db865a1
James Hudnall <jimmyhudnall@gmail.com>
2020-10-29 21:19:04 -0400

    setup Plotly data flow between front and back ends

--- website/world-bank-example/worldbankapp/routes.py
+++ website/world-bank-example/worldbankapp/routes.py
@@ -1,12 +1,43 @@
 from worldbankapp import app
 from flask import render_template
 from wrangling_scripts.wrangling import data_wrangling
+import pandas as pd 
+from wrangling_scripts.wrangling import data_wrangling
+
+import plotly.graph_objs as go 
+import plotly, json 
 
 data = data_wrangling()
+print(data[0])
 
-print(data)
+country = data[0][0]
+x_val = data[0][1]
+y_val = data[0][2]
+
+graph_one = [go.Scatter(
+    x = data[0][1],
+    y = data[0][2],
+    mode = 'lines',
+    name = country
+)]
+
+layout_one = dict(title = 'Change in Hectares Arable Land <br> per Person 1990 to 2105',
+    xaxis = dict(title = 'Year',
+        autotick = False, tick0 = 1990, dtick = 25),
+        yaxis = dict(title = 'Hectares')
+        )
+
+figures = []
+figures.append(dict(data=graph_one, layout=layout_one))
+
+# plot IDs for the html ID tag
+ids = [f'figure-{i}' for i, _ in enumerate(figures)]
+
+# Convert the plotly figures to JSON for javascript in html template
+figuresJSON = json.dumps(figures, cls = plotly.utils.PlotlyJSONEncoder)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', data_set = data)
+    return render_template('index.html', ids=ids, figuresJSON=figuresJSON)
